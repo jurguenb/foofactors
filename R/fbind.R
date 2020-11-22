d6f59c4266fffd364b5e1d40d95268796c058309
jurguenb <jurguenb@gmai.com>
2020-11-21 18:34:46 -0600

    building of fbind

---
+++ R/fbind.R
@@ -0,0 +1,16 @@
+#' Bind two factos
+#'
+#'Create a new factor from two existing factors, where the new factor's levels
+#'Create a new factor from two existing factors, where the new factor's levels
+#'
+#' @param a factor
+#' @param b factor
+#'
+#' @return factor
+#' @export
+#'
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
+fbind <- function(a, b) {
+  factor(c(as.character(a), as.character(b)))
+}
